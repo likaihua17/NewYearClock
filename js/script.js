ee87df9552bd2293ab64fdc4bb490b02fa249aed
likaihua17 <likaihua.cn@outlook.com>
2024-12-31 20:13:33 +0800

    bugfix

--- js/script.js
+++ js/script.js
@@ -78,7 +78,7 @@ const mainStage = new Stage("main-canvas");
 const stages = [trailsStage, mainStage];
 
 //随机文字烟花内容
-const randomWords = ["新年快乐", "心想事成", "陈新平"];
+const randomWords = ["新年快乐", "平安喜乐", "陈新平", "李凯华", "2025", "身体健康"];
 const wordDotsMap = {};
 randomWords.forEach((word) => {
 	wordDotsMap[word] = MyMath.literalLattice(word, 3, "Gabriola,华文琥珀", "90px");
